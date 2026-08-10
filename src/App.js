acb3f05532fa8e4753c6063a2d4d1c6fe4b48410
omnanamy2436 <omnanamy2436@gmail.com>
2026-08-10 11:20:36 +0700

    Create State-Function

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import RefsFunc from './refs-func';
-import RefsArray from './refs-array';
+import MessageBox from './state-func';
 export default function App() {
-  return <RefsArray/>;
+  return <MessageBox/>;
 }
